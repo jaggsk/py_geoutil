a435062ea10fb97968a16ab35147227aae843dbd
Jaggs <kevin.jaggs@ineos.com>
2026-03-11 10:33:42 +0000

    Add main Init

---
+++ src/py_geoutil/__init__.py
@@ -0,0 +1,7 @@
+from .coordinates.rotate_coords import rotate_points, create_grid_points
+#from .variogram.normal_score_transform import  NormalScoreTransform
+#from .variogram.poly_trend import polynomial_detrend
+
+__all__ =  ["rotate_points","create_grid_points"]
+
+__version__ = "0.0.1"
