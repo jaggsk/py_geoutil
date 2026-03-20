01972f8fbdb942845d8c48b9412e1c4852c2c810
Jaggs <kevin.jaggs@ineos.com>
2026-03-20 13:40:53 +0000

    Updated coordinate rotations with meshgrid check

--- src/py_geoutil/__init__.py
+++ src/py_geoutil/__init__.py
@@ -1,7 +1,7 @@
-from .coordinates.rotate_coords import rotate_points, create_grid_points
+from .coordinates.rotate_coords import rotate_points, create_grid_points, meshgrid_properties
 #from .variogram.normal_score_transform import  NormalScoreTransform
 #from .variogram.poly_trend import polynomial_detrend
 
-__all__ =  ["rotate_points","create_grid_points"]
+__all__ =  ["rotate_points","create_grid_points","meshgrid_properties"]
 
 __version__ = "0.0.1"
