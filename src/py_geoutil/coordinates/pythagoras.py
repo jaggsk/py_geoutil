6a3b617542cbe8f02eb0d2b1715b9d8f288e25c3
Jaggs <kevin.jaggs@ineos.com>
2026-06-16 16:05:35 +0100

    added pythagoras

---
+++ src/py_geoutil/coordinates/pythagoras.py
@@ -0,0 +1,13 @@
+def pythagoras_calc(Xone=None,Xtwo=None,Yone=None, Ytwo=None):
+    '''
+    Calculates distance between 2 xy coordinate pairs
+
+    Xone (Float): x coordinate of first pair 
+    Yone (Float): y coordinate of first pair 
+    Xtwo (Float): x coordinate of second pair 
+    Ytwo (Float): y coordinate of second pair 
+    Raises: None
+    
+    K Jaggs June 2026
+    '''
+    return (((Xone-Xtwo)**2) + ((Yone-Ytwo)**2))**0.5
